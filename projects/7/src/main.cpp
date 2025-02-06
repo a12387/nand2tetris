c66f90dd91f818c0524897b60ea20a2f62e3de6d
a12387 <1364219910@qq.com>
2025-02-06 23:49:27 +0800

    VM translator Pt.II Complete -The test script had a bug so I fixed it

--- projects/7/src/main.cpp
+++ projects/7/src/main.cpp
@@ -14,11 +14,14 @@ void parse(const std::string &input, std::string &inst, std::string &seg,
 
 void write(std::ifstream &in, std::ofstream &out, std::string &name) {
     std::string s;
-    int labelno = 0;
+    int labelno = 0, retno = 0;
+    std::string funcName = "";
     while (std::getline(in, s)) {
-        if (s.find("//") != std::string::npos) {
-            continue;
-        } else if (s.find_first_not_of(' ') == std::string::npos) {
+        size_t slash;
+        if ((slash = s.find("//")) != std::string::npos) {
+            s = s.substr(0, slash);
+        }
+        if (s.find_first_not_of(' ') == std::string::npos) {
             continue;
         }
 
@@ -126,7 +129,7 @@ void write(std::ifstream &in, std::ofstream &out, std::string &name) {
                 out << "M=D|M\n";
             else {
                 out << "D=M-D\n";
-                out << "@.TRUE" << labelno << "\n";
+                out << "@" << funcName << "$TRUE" << labelno << "\n";
                 if (inst == "eq")
                     out << "D;JEQ\n";
                 else if (inst == "gt")
@@ -136,13 +139,13 @@ void write(std::ifstream &in, std::ofstream &out, std::string &name) {
                 out << "@SP\n";
                 out << "A=M-1\n";
                 out << "M=0\n";
-                out << "@.END" << labelno << "\n";
+                out << "@" << funcName << "$END" << labelno << "\n";
                 out << "0;JMP\n";
-                out << "(.TRUE" << labelno << ")\n";
+                out << "(" << funcName << "$TRUE" << labelno << ")\n";
                 out << "@SP\n";
                 out << "A=M-1\n";
                 out << "M=-1\n";
-                out << "(.END" << labelno << ")\n";
+                out << "(" << funcName << "$END" << labelno << ")\n";
                 labelno++;
             }
         } else if (inst == "neg" || inst == "not") {
@@ -152,6 +155,135 @@ void write(std::ifstream &in, std::ofstream &out, std::string &name) {
                 out << "M=-M\n";
             else
                 out << "M=!M\n";
+        } else if (inst == "label") {
+            out << "(" << funcName << "$" << seg << ")\n";
+        } else if (inst == "goto") {
+            out << "@" << funcName << "$" << seg << "\n";
+            out << "0;JMP\n";
+        } else if (inst == "if-goto") {
+            out << "@SP\n";
+            out << "AM=M-1\n";
+            out << "D=M\n";
+            out << std::format("@{}${}\n", funcName, seg);
+            out << "D;JNE\n";
+        } else if (inst == "function") {
+            funcName = seg;
+            labelno = 0;
+            retno = 0;
+            out << std::format("({})\n", funcName);
+            for (int i = 0; i < std::stoi(index); i++) {
+                out << "@0\n";
+                out << "D=A\n";
+                out << "@SP\n";
+                out << "A=M\n";
+                out << "M=D\n";
+                out << "@SP\n";
+                out << "M=M+1\n";
+            }
+        } else if (inst == "call") {
+            // push @returnAddr, LCL, ARG, THIS, THAT
+            out << std::format("@{}$ret.{}\n", funcName, retno);
+            out << "D=A\n"
+                << "@SP\n"
+                << "A=M\n"
+                << "M=D\n"
+                << "@SP\n"
+                << "M=M+1\n";
+            for (auto s : {std::string("LCL"), std::string("ARG"),
+                           std::string("THIS"), std::string("THAT")}) {
+                out << std::format("@{}\n", s);
+                out << "D=M\n"
+                    << "@SP\n"
+                    << "A=M\n"
+                    << "M=D\n"
+                    << "@SP\n"
+                    << "M=M+1\n";
+            }
+
+            // ARG = SP-5-nArgs
+            out << "@SP\n"
+                << "D=M\n"
+                << "@5\n"
+                << "D=D-A\n"
+                << "@" << index << "\n"
+                << "D=D-A\n"
+                << "@ARG\n"
+                << "M=D\n";
+
+            // LCL = SP
+            out << "@SP\n"
+                << "D=M\n"
+                << "@LCL\n"
+                << "M=D\n";
+
+            // goto functionName
+            out << "@" << seg << "\n"
+                << "0;JMP\n";
+
+            // (returnAddr)
+            out << std::format("({}$ret.{})\n", funcName, retno);
+            retno++;
+        } else if (inst == "return") {
+            // retAddr = *(endFrame - 5)
+            out << "@LCL\n"
+                << "D=M\n"
+                << "@5\n"
+                << "A=D-A\n"
+                << "D=M\n"
+                << "@R14\n"
+                << "M=D\n";
+
+            // *ARG = pop()
+            out << "@SP\n";
+            out << "AM=M-1\n";
+            out << "D=M\n";
+            out << "@ARG\n";
+            out << "A=M\n";
+            out << "M=D\n";
+
+            // SP = ARG + 1
+            out << "@ARG\n"
+                << "D=M+1\n"
+                << "@SP\n"
+                << "M=D\n";
+
+            // THAT = *(endFrame - 1)
+            out << "@LCL\n"
+                << "D=M\n"
+                << "@1\n"
+                << "A=D-A\n"
+                << "D=M\n"
+                << "@THAT\n"
+                << "M=D\n";
+            // THIS = *(endFrame - 2)
+            out << "@LCL\n"
+                << "D=M\n"
+                << "@2\n"
+                << "A=D-A\n"
+                << "D=M\n"
+                << "@THIS\n"
+                << "M=D\n";
+            // ARG = *(endFrame - 3)
+            out << "@LCL\n"
+                << "D=M\n"
+                << "@3\n"
+                << "A=D-A\n"
+                << "D=M\n"
+                << "@ARG\n"
+                << "M=D\n";
+            // LCL = *(endFrame - 4)
+            out << "@LCL\n"
+                << "D=M\n"
+                << "@4\n"
+                << "A=D-A\n"
+                << "D=M\n"
+                << "@LCL\n"
+                << "M=D\n";
+
+            // goto retAddr
+            out << "@R14\n"
+                << "A=M\n"
+                << "0;JMP\n";
         }
     }
     in.close();
@@ -167,8 +299,7 @@ int main(int argc, char **argv) {
     if (std::filesystem::is_regular_file(path)) {
         std::ifstream in(path, std::ios::in);
         std::string name = path.stem().string();
-        std::filesystem::path outFile =
-            path.replace_extension("asm");
+        std::filesystem::path outFile = path.replace_extension("asm");
         std::ofstream out(outFile, std::ios::out | std::ios::trunc);
         write(in, out, name);
         out.close();
@@ -177,6 +308,8 @@ int main(int argc, char **argv) {
         outFile.replace_filename(outFile.parent_path().filename());
         outFile.replace_extension("asm");
         std::ofstream out(outFile, std::ios::out | std::ios::trunc);
+        out << "@Sys.init\n"
+            << "0;JMP\n";
         for (auto dir_entry : std::filesystem::directory_iterator(path)) {
             if (dir_entry.path().extension() == ".vm") {
                 std::ifstream in(dir_entry.path(), std::ios::in);
